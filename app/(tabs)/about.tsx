bbc75cd9b287afea0c29f45f233b9908d4ade366
Hojin <wkdghwls0623@gmail.com>
2024-11-13 01:23:20 +0900

    feat: natviewind apply

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -3,7 +3,9 @@ import { Text, View, StyleSheet } from 'react-native';
 export default function AboutScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About screen</Text>
+      <Text style={styles.text} className="bg-red-500">
+        About screen
+      </Text>
     </View>
   );
 }
